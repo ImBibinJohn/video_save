200a07cb7c2040beb6949113b56dd5db43435b0a
Bibin John <bibinjohnsysitco@gmail.com>
2023-06-13 17:14:34 +0530

    migration changes

--- App/models.py
+++ App/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
-    video_file = models.FileField(upload_to='videos/')
+    video_file = models.FileField()
